54ed3f6eb290d5782c358405ee18f7af10aaef0d
Bryan Dumpit <dumpitbr@gmail.com>
2022-09-20 05:43:02 -0400

    added voter routes

--- routes/apiRoutes/candidateRoutes.js
+++ routes/apiRoutes/candidateRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../../db/connection');
-const inputCheck = require('../..utils/inputCheck');
+const inputCheck = require('../../utils/inputCheck');
 
 router.get('/candidates', (req,res) => {
     const sql = `SELECT candidates.*, parties.name
